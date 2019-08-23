118beb45db50e951732040d48018f9916e5f471c
uym2 <ceciliamaitouyen@gmail.com>
2019-08-23 10:40:34 -0700

    a script to randomly reroot a tree

---
+++ util_scripts/random_reroot.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+inTree=$1
+outTree=$2
+L=`nw_labels $inTree`
+R=`nw_labels -r $inTree`
+N=1
+
+for x in $L; do
+    if [[ $x != $R ]]; then 
+        r=`echo $RANDOM/32768*$N | bc -l`
+        if (( $(echo "$r <= 1" | bc -l) )) ; then
+            y=$x
+        fi   
+        N=$((N+1))
+    fi    
+done
+echo $y
+nw_reroot $inTree $y > $outTree        
+
