e21400439b05debf146ab320d8e7255732ed3608
Mai To Uyen <uym2@Mais-MacBook-Pro.local>
2019-08-25 00:58:05 -0700

    Updated results

--- analyses/Bio_Data/run_analysis.sh
+++ analyses/Bio_Data/run_analysis.sh
@@ -4,11 +4,23 @@ data=$1
 
 cd $data
 
-tripRoot $data-taxonomy.tre $data.trees $data\_tripRoot.trees > $data\_tripRoot.log 2>&1 &
-python ~/Documents/my_gits/MinVar-Rooting/FastRoot.py -i $data.trees -o $data\_MV.trees -m MV &
-python ~/Documents/my_gits/MinVar-Rooting/FastRoot.py -i $data.trees -o $data\_MP.trees -m MP &
+echo "Running tripRoot ..."
+tripRoot $data-taxonomy.tre $data.trees $data\_tripRoot.trees > $data\_tripRoot.log 2>&1 
 
-method=tripRoot; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1 &
-method=MP; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1 &
-method=MV; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1 &
-method=OG; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1 &
+#echo "Running MV ..."
+#python ~/Documents/my_gits/MinVar-Rooting/FastRoot.py -i $data.trees -o $data\_MV.trees -m MV 
+
+#echo "Running MP ..."
+#python ~/Documents/my_gits/MinVar-Rooting/FastRoot.py -i $data.trees -o $data\_MP.trees -m MP 
+
+echo "Running TreeCmp - tripRoot ..."
+method=tripRoot; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1
+
+#echo "Running TreeCmp - MP ..."
+#method=MP; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1 &
+
+#echo "Running TreeCmp - MV ..."
+#method=MV; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1 &
+
+#echo "Running TreeCmp - OG ..."
+#method=OG; java -jar ~/Documents/Packages_N_Libraries/TreeCmp/bin/TreeCmp.jar -m -d mc -i $data\_$method.trees -o $data\_$method.dmc_pairs -N -P > $method_dmc.log 2>&1 &
