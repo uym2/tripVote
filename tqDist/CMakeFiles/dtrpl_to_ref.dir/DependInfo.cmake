839d86090cd05db6b9a32dfc4389156eeeee3eb2
Mai To Uyen <uym2@Mais-MBP.attlocal.net>
2019-09-08 23:08:51 -0700

    Added triplet_dist_to_ref.cpp

---
+++ tqDist/CMakeFiles/dtrpl_to_ref.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/AbstractDistanceCalculator.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/AbstractDistanceCalculator.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDT.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDT.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDTCountingCCToC.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDTCountingCCToC.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDTCountingCTransform.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDTCountingCTransform.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDTCountingG.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDTCountingG.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDTCountingIGToC.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDTCountingIGToC.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDTCountingLeaf.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDTCountingLeaf.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDTFactory.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDTFactory.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/HDTListUtils.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/HDTListUtils.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/NewickParser.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/NewickParser.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/QuartetDistanceCalculator.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/QuartetDistanceCalculator.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/RootedTree.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/RootedTree.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/RootedTreeFactory.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/RootedTreeFactory.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/TripletDistanceCalculator.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/TripletDistanceCalculator.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/int_stuff.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/int_stuff.cpp.o"
+  "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/triplet_dist_to_ref.cpp" "/Users/uym2/Documents/my_gits/Triplet_rooting/tqDist/CMakeFiles/dtrpl_to_ref.dir/triplet_dist_to_ref.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "N4INT128"
+  "doExtractAndContract"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tqDist/CMakeFiles/triplet_dist_2_ref.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "../bin/triplet_dist_2_ref"
+  "../bin/triplet_dist_2_ref.pdb"
+  "CMakeFiles/triplet_dist_2_ref.dir/AbstractDistanceCalculator.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDT.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDTCountingCCToC.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDTCountingCTransform.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDTCountingG.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDTCountingIGToC.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDTCountingLeaf.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDTFactory.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/HDTListUtils.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/NewickParser.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/QuartetDistanceCalculator.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/RootedTree.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/RootedTreeFactory.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/TripletDistanceCalculator.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/int_stuff.cpp.o"
+  "CMakeFiles/triplet_dist_2_ref.dir/triplet_dist_to_ref.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/triplet_dist_2_ref.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
