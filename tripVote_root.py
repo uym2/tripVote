29d892731090f6347fc6584d937c7294951c98ce
Uyen Mai <ceciliamaitouyen@gmail.com>
2021-04-14 20:38:10 -0700

    Update tripVote_root.py

--- tripVote_root.py
+++ tripVote_root.py
@@ -7,7 +7,7 @@ import time
 from tripVote.tripvote_lib import *
 
 def main():
-    MY_VERSION='1.0.4b'
+    MY_VERSION='1.0.4'
 
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input', required=True, help="Input Unrooted Trees")
