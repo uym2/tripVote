4fc805b463a5459ea5486fad9748ca2b6e2d9bd3
Mai To Uyen <uym2@Mais-MBP-3.attlocal.net>
2021-04-23 10:18:31 -0700

    remove old files

--- run_test_cases.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo Step1: running MinVar
-./bin/MVRoot test_cases/unrooted.trees test_cases/MV_rooted.trees
-
-echo Step2: running all-pairs tripRoot
-./run_tripVote.sh test_cases/MV_rooted.trees test_cases/wMVvote_rooted.trees test_cases/MV_rooted.trees y
